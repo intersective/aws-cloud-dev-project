87f02cb92dbb3ad9273f2840a823c8321893f51f
sunil <sunil@practera.com>
2024-01-12 09:28:02 +0530

    fix manual deploy dcript to run one instance

--- zap-proxy/deploy-wes.sh
+++ zap-proxy/deploy-wes.sh
@@ -11,12 +11,12 @@ deploy_stack() {
     sam deploy \
         --template-file stack.yml \
         --stack-name $StackName \
-        --s3-bucket cybersec-deployment-files \
+        --resolve-s3 \
         --debug \
         --capabilities CAPABILITY_NAMED_IAM \
-        --profile cyber-sandbox \
+        --profile zap \
         --region us-east-1 \
-        --parameter-overrides "AppImageUrl=$APP_URI" "NginxImageUrl=$NGNIX_URI TeamName=$TeamName SubDomainName=$SubDomainName SSLCertificateArn=$CERTIFICATE_ARN RootDomainName=$RootDomainName"
+        --parameter-overrides "AppImageUrl=$APP_URI" "Client=$Client" "NginxImageUrl=$NGNIX_URI TeamName=$TeamName SubDomainName=$SubDomainName SSLCertificateArn=$CERTIFICATE_ARN RootDomainName=$RootDomainName"
 
     # Check the deployment status
     if [ $? -ne 0 ]; then
@@ -28,12 +28,12 @@ deploy_stack() {
 # Set NGINX_IMAGE_URI and other variables here
 export NGNIX_URI=510645120987.dkr.ecr.us-east-1.amazonaws.com/nginx
 export CERTIFICATE_ARN=arn:aws:acm:us-east-1:510645120987:certificate/9e8615de-927e-427c-8e44-31e980de1de5
-export APP_URI=510645120987.dkr.ecr.us-east-1.amazonaws.com/zaproxy
-    
+export APP_URI=ghcr.io/zaproxy/zaproxy:stable
+
 # Teams for WBLA
-for ((i=1; i<=10; i++)); do
-    TeamName="team-$i"
-    Client="wbla"
+for ((i=1; i<=1; i++)); do
+    TeamName="westest-$i"
+    Client="attempt"
     SubDomainName="$TeamName-$Client.cybersec.practeraco.de"
     StackName="$TeamName-$Client"
     RootDomainName="cybersec.practeraco.de"
@@ -41,13 +41,13 @@ for ((i=1; i<=10; i++)); do
     deploy_stack "$TeamName" "$Client" "$SubDomainName" "$StackName" "$RootDomainName"
 done
 
-# Teams for IBM
-for ((i=1; i<=10; i++)); do
-    TeamName="team-$i"
-    Client="ibm"
-    SubDomainName="$TeamName-$Client.cybersec.practeraco.de"
-    StackName="$TeamName-$Client"
-    RootDomainName="cybersec.practeraco.de"
+# # Teams for IBM
+# for ((i=1; i<=10; i++)); do
+#     TeamName="team-$i"
+#     Client="ibm"
+#     SubDomainName="$TeamName-$Client.cybersec.practeraco.de"
+#     StackName="$TeamName-$Client"
+#     RootDomainName="cybersec.practeraco.de"
 
-    deploy_stack "$TeamName" "$Client" "$SubDomainName" "$StackName" "$RootDomainName"
-done
+#     deploy_stack "$TeamName" "$Client" "$SubDomainName" "$StackName" "$RootDomainName"
+# done
